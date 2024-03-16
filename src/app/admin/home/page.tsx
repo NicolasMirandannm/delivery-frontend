192f8a161b9d11e0ec99d6df49228c1ca484eac6
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-03-16 14:59:58 -0400

    menu layout created

---
+++ src/app/admin/home/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { MenuLayout } from '@/app/components/menu-layout/menu-layout';
+
+export default function AdminHomePage() {
+  return (
+    <MenuLayout>
+      <div>Teste</div>
+    </MenuLayout>
+  );
+}
